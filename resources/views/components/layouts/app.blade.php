9188ca49e89a969d89a25caa55e1f164670e22f2
Ivan Tanjaya <ivantanjaya07@gmail.com>
2024-05-02 15:00:44 +0700

    New commit

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -24,8 +24,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body>  
-    <div class="cryptohopper-web-widget" data-id="2" data-coins="bitcoin,ethereum,tether,bnb,solana,usd-coin,xrp,staked-ether,dogecoin,lido-staked-ether,the-open-network,cardano,shiba-inu,gaj,avalanche-2,wrapped-steth,tron,polkadot,wrapped-bitcoin,bitcoin-cash,near-protocol,bitcoin-cash-sv,chainlink,matic-network,near,internet-computer,litecoin,leo-token,uniswap,dai,first-digital-usd" data-realtime="on" data-currency="IDR"></div>
+<body>
+    <div class="cryptohopper-web-widget" data-id="2" data-currency="IDR"></div>  
     @yield("container")
     @livewireScripts
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
